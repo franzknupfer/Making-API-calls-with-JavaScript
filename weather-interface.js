fdeab85388ce7af22a6fbffdcb0345daa954c43f
Franz Knupfer <franzknupfer@hotmail.com>
2017-08-10 10:34:05 -0700

    API call with jQuery get and promise

--- weather-interface.js
+++ weather-interface.js
@@ -1,22 +1,14 @@
-let apiKey = "[API-KEY-GOES-HERE]";
+let apiKey = "64ce943b2cb480de60202f7a5a15b25c";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
 });
